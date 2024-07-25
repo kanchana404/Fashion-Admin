b1391f27d891f7190ad624c7eed894fd111b3660
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-07-26 00:50:22 +0530

    chore: Update upload.svg file

--- app/(root)/add-product/page.tsx
+++ app/(root)/add-product/page.tsx
@@ -1,34 +1,106 @@
+"use client";
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const page = () => {
+const AddProductPage: React.FC = () => {
+  const [selectedImages, setSelectedImages] = useState<string[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const fileArray = Array.from(files).map(file => {
+        const reader = new FileReader();
+        return new Promise<string>((resolve, reject) => {
+          reader.onload = () => resolve(reader.result as string);
+          reader.onerror = error => reject(error);
+          reader.readAsDataURL(file);
+        });
+      });
+
+      Promise.all(fileArray).then(images => {
+        setSelectedImages(prevImages => [...prevImages, ...images]);
+      });
+    }
+  };
+
+  const triggerFileInput = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <>
       <div>
         <Navbar />
-
-        <div className="md:w-4/6 w-full">
-          <div className=" flex ">
-            <div className="p-6 space-x-5 w-full  flex">
-              <Input className="w-2/3" placeholder="Product Name" />
-              <div className="relative w-1/3">
-                <input
-                  className="w-full pl-10 pr-2 py-2 border rounded"
-                  type="text"
-                  placeholder="Price"
-                />
-                <div className="absolute inset-y-0 left-0 flex items-center pl-2">
-                  <Image src="/dollar.png" alt="logo" width={20} height={20} />
+        <div className="flex flex-col lg:flex-row justify-between">
+          <div className="md:w-full lg:w-4/6">
+            <div className="flex flex-col lg:flex-row">
+              <div className="p-6 w-full flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5">
+                <Input className="w-full lg:w-2/3" placeholder="Product Name" />
+                <div className="relative w-full lg:w-1/3">
+                  <input
+                    className="w-full pl-10 pr-2 py-2 border rounded"
+                    type="text"
+                    placeholder="Price"
+                  />
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-2">
+                    <Image
+                      src="/dollar.png"
+                      alt="logo"
+                      width={20}
+                      height={20}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="p-6">
+              <Textarea rows={8} placeholder="Description" />
+            </div>
           </div>
-
-          <div className=" p-6">
-            <Textarea rows={8} />
+          <div className="p-6 w-full lg:w-2/6 flex justify-center items-center">
+            <div className="border p-4 rounded-lg flex flex-col items-center h-auto">
+              <Input
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={handleImageUpload}
+                className="hidden"
+                ref={fileInputRef}
+              />
+              <div className="flex flex-col items-center">
+                <Image
+                  className="mt-8"
+                  src="/upload.svg"
+                  alt="upload icon"
+                  width={60}
+                  height={60}
+                />
+                <Button className="mt-4 bg-yellow-400 w-32" onClick={triggerFileInput}>
+                  Upload
+                </Button>
+              </div>
+              {selectedImages.length > 0 && (
+                <div className="mt-4 flex flex-wrap gap-4">
+                  {selectedImages.map((image, index) => (
+                    <Image
+                      key={index}
+                      src={image}
+                      alt={`Selected Image ${index + 1}`}
+                      width={100}
+                      height={100}
+                      className="rounded"
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -36,4 +108,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default AddProductPage;
