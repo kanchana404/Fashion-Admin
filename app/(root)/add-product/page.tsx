d2ca98e34001971ead42fa9cdf69cf01c3f694a6
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-07-26 00:13:05 +0530

    chore: Update page layout and add Card component for adding a new product

--- app/(root)/add-product/page.tsx
+++ app/(root)/add-product/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Navbar from "@/components/Navbar";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div>
+        <Navbar />
 
-export default page
+        <div className="md:w-4/6 w-full">
+          <div className=" flex ">
+            <div className="p-6 space-x-5 w-full  flex">
+              <Input className="w-2/3" placeholder="Product Name" />
+              <div className="relative w-1/3">
+                <input
+                  className="w-full pl-10 pr-2 py-2 border rounded"
+                  type="text"
+                  placeholder="Price"
+                />
+                <div className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <Image src="/dollar.png" alt="logo" width={20} height={20} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className=" p-6">
+            <Textarea rows={8} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
