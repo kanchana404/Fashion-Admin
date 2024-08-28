624b48fefa8244431ab1ce423cc437a8b9ee8295
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-08-28 23:39:52 +0530

    refactor: Update Navbar to include a logo image using next/image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex'>
+        <Image
+        className='p-4'
+        src="/Brand.png"
+        alt="Picture of the author"
+        width={200}
+        height={200}
+        />
+
+      {/* test */}
+    </div>
   )
 }
 
