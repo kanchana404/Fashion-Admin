3020987a2f16e9da8884a53c98d0b2108aa85e38
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-08-30 23:43:27 +0530

    refactor: Update image handling in Home page
    - Changed the state variable `imageUrl` to `imageUrls`, which is now an array of strings to store multiple image URLs.
    - Updated the `handleUploadComplete` function to append the new image URLs to the `imageUrls` array.
    - Modified the form validation in the `handleSubmit` function to check if at least one image has been uploaded.
    - Updated the API route to use the `imageUrls` array instead of a single `imageUrl` for creating a product.
    - Updated the `Product` model to store the image URLs as an array of strings.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <Navbar />
+      <Sidebar />
     </div>
   )
 }
---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+export const NAV_LINKS = [
+    { href: "/", key: "DASHBOARD", label: "DASHBOARD" , scr:"/Home.png"},
+    { href: "/customers", key: "CUSTOMERS", label: "CUSTOMERS", scr:"/Users.png" },
+    { href: "/analytics", key: "ANALYTICS", label: "ANALYTICS" , scr:"/Pie Chart.png"},
+    { href: "/messages", key: "MESSAGES ", label: "MESSAGES ", scr:"/Mail.png" },
+    { href: "/setting", key: "SETTING ", label: "SETTING" , scr:"/Setting.png"},
+    { href: "/help-center", key: "HELP ", label: "HELP CENTER ", scr:"/Help.png" },
+  ];
