a2d40622072ba90b8f395cc3d4478061cbbe940c
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-08-31 22:54:38 +0530

    refactor: Update Customers page layout and styling
    - Updated the layout of the Customers page to use a flexbox container with a column layout on small screens and a row layout on large screens.
    - Adjusted the font size of the page title to be responsive and more visually appealing.
    - Improved the breadcrumb navigation by adding proper spacing and alignment.

--- app/(root)/customers/page.tsx
+++ app/(root)/customers/page.tsx
@@ -11,26 +11,29 @@ import {
 
 const page = () => {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex flex-col lg:flex-row">
+  <Sidebar/>
 
-      <div className="block">
-        <h1 className="text-7xl p-4 mt-16">Customers List</h1>
-        <div className="ml-7">
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Customer List</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </div>
+  <div className="w-full lg:w-3/4">
+    <h1 className="text-3xl md:text-5xl lg:text-7xl p-4 mt-8 lg:mt-16">
+      Customers List
+    </h1>
+    <div className="ml-4 md:ml-7">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Customer List</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
     </div>
+  </div>
+</div>
+
   );
 };
 
